6db98999a99950e1ad719e93c9f3cc02c05c7b81
zainkhan <khanzain33999@gmail.com>
2023-09-24 09:58:06 +0500

    <commit-message>

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 const header = document.querySelector("header")
 window.addEventListener ("scroll",()=>{
-    header.classList.toggle("sticky",window.scrollY)
+    header.classList.toggle("sticky",window.scroll)
 })
