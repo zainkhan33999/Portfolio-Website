e23b3904eb37227cf8a12cd7e70f665bcc26b989
zainkhan <khanzain33999@gmail.com>
2024-08-02 01:20:13 +0500

    hmm

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
-const header = document.querySelector("header")
+const header = document.querySelector('header');
+const list = document.querySelector('ul');
+const hamburger = document.querySelector('#menu-icon');
+const moblist = document.querySelector('.mob-list');
+
+// Function to toggle sticky class
+function toggleSticky() {
+  header.classList.toggle('sticky');
+  list.classList.toggle('mob-list');
+  
+}
 window.addEventListener ("scroll",()=>{
     header.classList.toggle("sticky",window.scroll)
 })
+
+// Add click event listener to hamburger menu
+hamburger.addEventListener('click', toggleSticky);
