803cf70524ce662e6508cdb819c7a3263db6c49a
zainkhan <khanzain33999@gmail.com>
2024-08-02 11:35:47 +0500

    completed

--- app.js
+++ app.js
@@ -5,13 +5,10 @@ const moblist = document.querySelector('.mob-list');
 
 // Function to toggle sticky class
 function toggleSticky() {
-  header.classList.toggle('sticky');
   list.classList.toggle('mob-list');
   
 }
-window.addEventListener ("scroll",()=>{
-    header.classList.toggle("sticky",window.scroll)
-})
+
 
 // Add click event listener to hamburger menu
 hamburger.addEventListener('click', toggleSticky);
